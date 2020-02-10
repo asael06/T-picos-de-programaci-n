b80f85047f4d8eeecea2336bbed074fb2f83793f
Asael Martínez <asael.mb@hotmail.com>
2020-02-10 13:16:20 -0600

    Se crearon las clases ClaseDatos y Seleccionador

---
+++ TAP_31ENE_10/src/Principal/Seleccionador.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Principal;
+
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JCheckBox;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author asael
+ */
+public class Seleccionador extends JFrame implements ActionListener{
+    
+    GridLayout grid = new GridLayout();
+    JCheckBox []arregloCheck;
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}
