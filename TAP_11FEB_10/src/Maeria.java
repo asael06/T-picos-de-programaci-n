ac6caf975e143b54d0f68ffc7e415114bde3d46e
Asael Martínez <asael.mb@hotmail.com>
2020-02-11 13:43:43 -0600

    Nuevo proyecto TAP TAP_11FEB_10

---
+++ TAP_11FEB_10/src/Maeria.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author asael
+ */
+public class Maeria {
+
+    public Maeria(String nombre, boolean stat) {
+        this.nombre = nombre;
+        this.stat = stat;
+    }
+
+    public Maeria() {
+    }
+    
+    String nombre;
+    boolean stat;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public boolean isStat() {
+        return stat;
+    }
+
+    public void setStat(boolean stat) {
+        this.stat = stat;
+    }   
+}
